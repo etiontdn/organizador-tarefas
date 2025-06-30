ed47ecc16d0e873c4944ec4ef4ca91be5a73d924
João J <etiontdn@gmail.com>
2025-06-30 12:29:58 -0300

    push backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'process.env.DATABASE_HOST',
+      port: parseInt(process.env.DATABASE_PORT || '', 10),
+      password: process.env.DATABASE_PASSWORD,
+      username: process.env.DATABASE_USER,
+      entities: ['src/**/*.entity{.ts,.js}'],
+      database: process.env.DATABASE_NAME,
+      synchronize: true,
+      logging: true,
+    }),
+    TasksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

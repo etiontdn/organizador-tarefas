229270b6b0f9bc02a6dcc3b69bb2bda5a4198582
João J <etiontdn@gmail.com>
2025-06-30 14:36:14 -0300

    fazendo entity

--- backend/src/tasks/entities/task.entity.ts
+++ backend/src/tasks/entities/task.entity.ts
@@ -1,1 +1,10 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  tarefa: string;
+}

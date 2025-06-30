f1cd74e5049a59d9b7c679e12af9c65d33072719
João J <etiontdn@gmail.com>
2025-06-30 14:46:22 -0300

    feito dto

--- backend/src/tasks/dto/update-task.dto.ts
+++ backend/src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, MaxLength, IsNotEmpty } from 'class-validator';
 import { CreateTaskDto } from './create-task.dto';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsString()
+  @MaxLength(255)
+  @IsNotEmpty()
+  tarefa: string;
+}

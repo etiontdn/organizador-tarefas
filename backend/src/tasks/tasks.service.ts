b10d5a115abb37546bd86c2c71dbe12ca9f55c25
João J <etiontdn@gmail.com>
2025-06-30 14:46:37 -0300

    feito módulo e service

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
+  constructor(
+    @InjectRepository(Task) private readonly taskRepository: Repository<Task>,
+  ) {}
+
   create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+    const task = new Task();
+    task.tarefa = createTaskDto.tarefa;
+    return this.taskRepository.save(task);
   }
 
   findAll() {
-    return `This action returns all tasks`;
+    return this.taskRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} task`;
+    return this.taskRepository.findOneBy({ id });
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+    const task = new Task();
+    task.id = id;
+    task.tarefa = updateTaskDto.tarefa;
+    return this.taskRepository.save(task);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} task`;
+    return this.taskRepository.delete(id);
   }
 }
